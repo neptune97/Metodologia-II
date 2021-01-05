8f2155922329fe51d4db8554cc82ae8c919fedd0
Fernando <57836771+neptune97@users.noreply.github.com>
2021-01-04 23:30:31 -0300

    Add files via upload

---
+++ Aula 01/Aula-01.R
@@ -0,0 +1,61 @@
+############################################
+#################################### AULA 01
+####################AUTOR: FERNANDO DE SOUZA
+
+# OPERAÇÕES MATEMÁTICAS
+
+5+5 # Adição
+5-5 # Subtração
+5*5 # Multiplicação
+5/5 # Divisão
+5^5 # Exponencial
+sqrt (5) # Raiz Quadrada
+log (5) # Logaritmo
+
+# CALCULANDO COM O R
+
+(2*4) + (8*6) / (5*8) - 9
+
+sqrt((2*4) + (8*6) / (5*8) - 9)
+
+# CRIANDOS OBJETOS
+
+x <- 6 
+x
+X
+
+# CRIANDO VETORES
+
+y <-  c(10, 30, 98, 45) #<<
+y
+b <- c(50, 100, 1000, 0)
+b
+
+# OPERAÇÕES COM VETORES
+
+y + b
+y * b
+y - b
+y / b
+
+# VETORES COM CHARACTER
+
+x <- c("janeiro", "fevereiro", "março")
+
+x
+
+# VALORES LÓGICOS
+
+5 > 4
+5 >= 10
+5 == 6 
+5 != 6 
+
+# COMANDOS BÁSICOS
+
+ls () #Lista objetos disponíveis
+q () #Fecha o R
+c () #Cria um vetor
+rm () #Remove objetos
+library () #Abre pacotes
+install.packages () #Instala pacotes
